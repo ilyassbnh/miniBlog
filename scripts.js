680ffdfd9989b132eed154db776f1cb4c5a1e2c4
ilyassbnh <benhida.ilyass@gmail.com>
2024-04-13 21:47:11 +0000

    popup update:

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-
 document.querySelectorAll('.popup .image').forEach(image => {
     image.onclick = () => {
-        document.querySelector('.popup .card').style.display = 'block';
-        document.querySelector('.popup .card img').src = image.querySelector('img').src;
+        image.nextElementSibling.style.display = 'block'; 
+    }
+});
+document.querySelectorAll('.popup span').forEach(closeButton => {
+    closeButton.onclick = () => {
+        closeButton.parentElement.style.display = 'none';
     }
 });
 
-document.querySelector('.popup span').onclick = () => {
-    document.querySelector('.popup').style.display = 'none';
-}
