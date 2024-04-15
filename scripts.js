cb1cfed222b0f0c4f77051736b2c3376d742a38a
ilyassbnh <benhida.ilyass@gmail.com>
2024-04-15 09:16:52 +0000

    responsive popup images update

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
-document.querySelectorAll('.post-image').forEach(image => {
-    image.onclick = () => {
-        image.parentElement.nextElementSibling.style.display = 'block'; 
-    }
-});
-document.querySelectorAll('.popup span').forEach(closeButton => {
-    closeButton.onclick = () => {
-        closeButton.parentElement.style.display = 'none';
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the popup image element
+    var popupImage = document.getElementById("popup-image");
+
+    // Get all clickable images
+    document.querySelectorAll('.post-image').forEach(image => {
+        image.onclick = () => {
+            // Get the source of the clicked image
+            var clickedImageSrc = image.querySelector("img").getAttribute("src");
+
+            // Update the source of the popup image
+            popupImage.setAttribute("src", clickedImageSrc);
+
+            // Show the popup
+            document.querySelector(".popup").style.display = "block";
+        }
+    });
+
+    // Add click event listener to close button
+    document.querySelector(".popup span").addEventListener("click", function() {
+        // Hide the popup
+        document.querySelector(".popup").style.display = "none";
+    });
 });
