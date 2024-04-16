50827b6e3ae73e4812a71bd1a1cb8769622f0986
ilyassbnh <benhida.ilyass@gmail.com>
2024-04-16 09:38:03 +0000

    javascript update

--- scripts.js
+++ scripts.js
@@ -1,24 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the popup image element
+document.addEventListener("DOMContentLoaded", function () {
     var popupImage = document.getElementById("popup-image");
-
-    // Get all clickable images
     document.querySelectorAll('.post-image').forEach(image => {
         image.onclick = () => {
-            // Get the source of the clicked image
             var clickedImageSrc = image.querySelector("img").getAttribute("src");
-
-            // Update the source of the popup image
             popupImage.setAttribute("src", clickedImageSrc);
-
-            // Show the popup
             document.querySelector(".popup").style.display = "block";
         }
     });
-
-    // Add click event listener to close button
-    document.querySelector(".popup span").addEventListener("click", function() {
-        // Hide the popup
+    document.querySelector(".popup span").addEventListener("click", function () {
         document.querySelector(".popup").style.display = "none";
     });
+    
+    // Get a reference to the like button element and the icon element
+    const likeButton = document.getElementById("like-button");
+    // Get all "like" buttons by their class name
+    const likeButtons = document.querySelectorAll(".like-button");
+
+    // Initialize a variable to track the like state for each item
+    const isLiked = Array.from(likeButtons).fill(false);
+
+    // Function to toggle the like state and update the button icon
+    function toggleLike(index) {
+        return function () {
+            const likeButton = likeButtons[index];
+            const likeIcon = likeButton.querySelector("i");
+
+            if (isLiked[index]) {
+                likeIcon.classList.remove("fas");
+                likeIcon.classList.add("far");
+                likeIcon.style.color = ""; // Remove the liked icon color
+            } else {
+                likeIcon.classList.remove("far");
+                likeIcon.classList.add("fas");
+                likeIcon.style.color = "#ff0000"; // Set the liked icon color to red
+            }
+            isLiked[index] = !isLiked[index];
+        };
+    }
+
+    // Add a click event listener to each "like" button
+    likeButtons.forEach((button, index) => {
+        button.addEventListener("click", toggleLike(index));
+    });
+
 });
